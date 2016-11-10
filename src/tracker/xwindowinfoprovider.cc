a3977a32e78f9c153ede5ae1df418e925cd72c04
Marcin Kolny <marcin.kolny@gmail.com>
2016-11-10 18:18:41 +0100

    fix segfault in xwindowinfoprovider

--- src/tracker/xwindowinfoprovider.cc
+++ src/tracker/xwindowinfoprovider.cc
@@ -20,62 +20,68 @@ static void strip_non_printable(std::string &str)
 
 std::string XWindowInfoProvider::get_window_title(Window win) const
 {
-    Window root = -1;
-    Window *children = nullptr;
-    unsigned int children_cnt;
-    int status;
     std::string ret;
 
-    do {
-        char *name;
-        status = XFetchName(display, win, &name);
-        if (status) {
-            ret = name;
-            XFree(name);
-            if (!ret.empty()) break;
-        }
-        XTextProperty text;
-        status = XGetWMName(display, win, &text);
-        if (status && text.nitems > 0) {
-            ret = reinterpret_cast<char*>(text.value);
-            XFree(text.value);
-        }
+    if (win)
+    {
+        Window root = -1;
+        Window *children = nullptr;
+        unsigned int children_cnt;
+        int status;
+
+        do {
+            char *name;
+            status = XFetchName(display, win, &name);
+            if (status) {
+                ret = name;
+                XFree(name);
+                if (!ret.empty()) break;
+            }
+            XTextProperty text;
+            status = XGetWMName(display, win, &text);
+            if (status && text.nitems > 0) {
+                ret = reinterpret_cast<char*>(text.value);
+                XFree(text.value);
+            }
+
+            XFree(children);
+        } while (ret.empty() && (status = XQueryTree(display, win, &root, &win, &children, &children_cnt)) && win != root);
 
         XFree(children);
-    } while (ret.empty() && (status = XQueryTree(display, win, &root, &win, &children, &children_cnt)) && win != root);
 
-    XFree(children);
-
-    strip_non_printable(ret);
-
+        strip_non_printable(ret);
+    }
     return ret.empty() ? std::string("(unknown)") : ret;
 }
 
 std::string XWindowInfoProvider::get_app_name(Window win) const
 {
-    int status;
-    XClassHint clh;
-    Window root = -1;
-    Window *children;
-    unsigned int children_cnt;
-
-    while (!(status = XGetClassHint(display, win, &clh)) && root != win) {
-        status = XQueryTree(display, win, &root, &win, &children, &children_cnt);
-        XFree(children);
-        if (!status) {
-            break;
-        }
-    }
-
     std::string ret;
 
-    if (status) {
-        ret = clh.res_name;
-        if (ret.empty()) {
-            ret = clh.res_class;
+    if (win)
+    {
+        int status;
+        XClassHint clh;
+        Window root = -1;
+        Window *children;
+        unsigned int children_cnt;
+
+        while (!(status = XGetClassHint(display, win, &clh)) && root != win) {
+            status = XQueryTree(display, win, &root, &win, &children, &children_cnt);
+            XFree(children);
+            if (!status) {
+                break;
+            }
         }
-        XFree(clh.res_name);
-        XFree(clh.res_class);
+
+        if (status) {
+            ret = clh.res_name;
+            if (ret.empty()) {
+                ret = clh.res_class;
+            }
+            XFree(clh.res_name);
+            XFree(clh.res_class);
+        }
     }
 
     if (ret.empty())
@@ -96,7 +102,6 @@ XWindowInfoProvider::Info XWindowInfoProvider::get_current_window_info() const
     int rev;
 
     XGetInputFocus(display, &window, &rev);
-
     return Info {get_window_title(window), get_app_name(window)};
 }
 

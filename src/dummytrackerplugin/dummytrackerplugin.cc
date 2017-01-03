ea241fad3465c4493059f899ae97c00bf278dd69
Marcin Kolny <marcin.kolny@gmail.com>
2017-01-03 21:43:52 +0100

    dummytrackerplugin: improve dummyplugin

--- src/dummytrackerplugin/dummytrackerplugin.cc
+++ src/dummytrackerplugin/dummytrackerplugin.cc
@@ -9,10 +9,13 @@
 #include "wtcommon/idatacontrolplugin.h"
 
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 struct WT_IDataControlPlugin
 {
     int cnt = 0;
+    bool silent = false;
 };
 
 static WT_IDataControlPlugin* create_plugin()
@@ -25,9 +28,16 @@ static void destroy_plugin(WT_IDataControlPlugin* obj)
     delete obj;
 }
 
-static void load_config(WT_IDataControlPlugin*, const char ***, int)
+static void load_config(WT_IDataControlPlugin* obj, const char ***config, int size)
 {
-
+    for (int i = 0; i < size; i++)
+    {
+        if (strcmp(config[0][i], "silent") == 0)
+        {
+            obj->silent = std::atoi(config[1][i]);
+            printf("Loaded configuration 'silent': %d\n", obj->silent);
+        }
+    }
 }
 
 static int plugin_process(WT_IDataControlPlugin* obj,
@@ -35,8 +45,11 @@ static int plugin_process(WT_IDataControlPlugin* obj,
                 char[WT_MAX_WIN_TITLE_LEN],
                 int*)
 {
-    obj->cnt++;
-    printf("%d \n", obj->cnt);
+    if (!obj->silent)
+    {
+        printf("I'm a dummy plugin, and I've run %d time(s).\n", ++obj->cnt);
+    }
+
     return 0;
 }
 
